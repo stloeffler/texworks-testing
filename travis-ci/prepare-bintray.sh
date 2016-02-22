e53916eeef01806dce86cf76491d538c2eed72fb
Stefan Löffler <st.loeffler@gmail.com>
2016-02-21 20:26:57 -0500

    Clean up travis-ci/prepare-bintray.sh

--- travis-ci/prepare-bintray.sh
+++ travis-ci/prepare-bintray.sh
@@ -1,22 +1,7 @@
 #!/usr/bin/env sh
 
-set -v
-
-pwd
-#cat src/TWVersion.h
-sed -nEe 's,^#define TEXWORKS_VERSION,,p' src/TWVersion.h
-sed -nEe 's,^#define TEXWORKS_VERSION\s,,p' src/TWVersion.h
-sed -nEe 's,^#define TEXWORKS_VERSION\t,,p' src/TWVersion.h
-sed -nEe 's,^#define TEXWORKS_VERSION\\s,,p' src/TWVersion.h
-sed -nEe 's,^#define TEXWORKS_VERSION\\t,,p' src/TWVersion.h
-sed -nEe 's,^#define TEXWORKS_VERSION\s\+,,p' src/TWVersion.h
-sed -nEe 's,^#define TEXWORKS_VERSION\s\+",,p' src/TWVersion.h
-sed -nEe 's,^#define TEXWORKS_VERSION\s"\?\([0-9.]\+\)"\?$,\1,p' src/TWVersion.h
-
-# GNU extensions are not supported; on Linux, use --posix
-sed -ne 's,^#define TEXWORKS_VERSION[[:space:]]"\([0-9.]\{3\,\}\)"$,\1,p' src/TWVersion.h
-
-TW_VERSION=$(sed -ne 's,^#define TEXWORKS_VERSION\s"\?\([0-9.]\+\)"\?$,\1,p' src/TWVersion.h)
+# GNU extensions for sed are not supported; on Linux, use --posix to mimick this behaviour
+TW_VERSION=$(sed -ne 's,^#define TEXWORKS_VERSION[[:space:]]"\([0-9.]\{3\,\}\)"$,\1,p' src/TWVersion.h)
 GIT_HASH=$(git --git-dir=".git" show --no-patch --pretty="%h")
 #DATE=$(date --rfc-3339="seconds")
 #DATE=$(date +"%Y-%m-%d %H:%M:%S%:z")
@@ -25,13 +10,13 @@ DATE=$(date -u +"%Y-%m-%dT%H:%M:%S%z")
 DATE_HASH=$(date -u +"%Y%m%d%H%M%S")
 
 #VERSION_NAME="TeXworks-${TRAVIS_OS_NAME}-${TW_VERSION}-${DATE_HASH}-git_${GIT_HASH}"
-
 VERSION_NAME="${TW_VERSION}-${DATE_HASH}-git_${GIT_HASH}"
 
 echo "TW_VERSION = ${TW_VERSION}"
 echo "GIT_HASH = ${GIT_HASH}"
 echo "DATE = ${DATE}"
 echo "DATE_HASH = ${DATE_HASH}"
+echo "VERSION_NAME = ${VERSION_NAME}"
 
 cat > travis-ci/bintray.json << EOF
 {
