e9fda21290cb2ac8f828b36ee0bb263ce5ee029d
Stefan Löffler <st.loeffler@gmail.com>
2016-02-22 12:08:19 -0500

    Outsource before_install and before_script to scripts (no DEPLOYMENT_TARGET anymore)
    travis-ci/get-dependencies.sh and travis-ci/configure.sh use fancy formatting and should produce less clutter in the build log
    
    CMAKE_OSX_DEPLOYMENT_TARGET='10.6' is not used anymore as homebrew'ed packages are built for current Mac OS X anyway (so only Maverick and up is supported ATM)

---
+++ travis-ci/configure.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env sh
+
+# Exit on errors
+set -e
+
+. $(dirname $0)/defs.sh
+
+print_headline "Configuring for ${TRAVIS_OS_NAME} with Qt${QT}"
+
+BUILDDIR="build-${TRAVIS_OS_NAME}-qt${QT}"
+
+print_info "Making build directory '${BUILDDIR}'"
+mkdir "${BUILDDIR}"
+cd "${BUILDDIR}"
+
+if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
+	print_info "Running CMake"
+	echo_and_run "cmake .. -DTW_BUILD_ID='travis-ci' -DCMAKE_INSTALL_PREFIX='/usr' -DDESIRED_QT_VERSION=\"$QT\""
+elif [ "${TRAVIS_OS_NAME}" = "osx" ]; then
+	if [ "${QT}" -eq 4 ]; then
+		print_info "Running CMake"
+		echo_and_run "cmake .. -DTW_BUILD_ID='travis-ci' -DDESIRED_QT_VERSION=\"$QT\" -DCMAKE_OSX_SYSROOT=macosx"
+	elif [ "${QT}" -eq 5 ]; then
+		print_info "Running CMake"
+		echo_and_run "cmake .. -DTW_BUILD_ID='travis-ci' -DDESIRED_QT_VERSION=\"$QT\" -DCMAKE_OSX_SYSROOT=macosx -DCMAKE_PREFIX_PATH=\"/usr/local/opt/qt5\""
+	else
+		print_error "Unsupported Qt version '${QT}'"
+		exit 1
+	fi
+	# -DCMAKE_OSX_DEPLOYMENT_TARGET='10.6'
+else
+	print_error "Unsupported operating system '${TRAVIS_OS_NAME}'"
+	exit 1
+fi
+
+print_info "Successfully configured build"
+
---
+++ travis-ci/get-dependencies.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env sh
+
+# Exit on errors
+set -e
+
+. $(dirname $0)/defs.sh
+
+print_headline "Getting dependencies for ${TRAVIS_OS_NAME} with Qt${QT}"
+
+if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
+	print_info "Updating apt cache"
+	sudo apt-get -qq update
+	if [ $QT -eq 4 ]; then
+		print_info "Installing packages: libqt4-dev zlib1g-dev libhunspell-dev libpoppler-qt4-dev liblua5.2-dev"
+		sudo apt-get -qq update && sudo apt-get install -y libqt4-dev zlib1g-dev libhunspell-dev libpoppler-qt4-dev liblua5.2-dev
+	elif [ $QT -eq 5 ]; then
+		print_info "Installing packages: qtbase5-dev qtscript5-dev qttools5-dev zlib1g-dev libhunspell-dev libpoppler-qt5-dev libpoppler-private-dev liblua5.2-dev"
+		sudo apt-get install -y qtbase5-dev qtscript5-dev qttools5-dev zlib1g-dev libhunspell-dev libpoppler-qt5-dev libpoppler-private-dev liblua5.2-dev;
+	else
+		print_error "Unsupported Qt version '${QT}'"
+		exit 1
+	fi
+elif [ "${TRAVIS_OS_NAME}" = "osx" ]; then
+	print_info "Updating homebrew"
+	brew update > brew_update.log || { print_error "Updating homebrew failed"; cat brew_update.log; exit 1; }
+	if [ $QT -eq 4 ]; then
+		print_info "Brewing packages: qt4 poppler hunspell lua"
+		brew install qt4
+		brew install poppler --with-qt --enable-xpdf-headers
+	elif [ $QT -eq 5 ]; then
+		print_info "Brewing packages: qt5 poppler hunspell lua"
+		brew install qt5
+		brew install poppler --with-qt5 --enable-xpdf-headers
+	else
+		print_error "Unsupported Qt version '${QT}'"
+		exit 1
+	fi
+	brew install hunspell
+	brew install lua;
+else
+	print_error "Unsupported operating system '${TRAVIS_OS_NAME}'"
+	exit 1
+fi
+
+print_info "Successfully set up dependencies"
