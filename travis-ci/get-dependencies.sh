e413fd338c5295426333a3bd156a670b76f4c066
Stefan Löffler <st.loeffler@gmail.com>
2016-02-27 18:17:15 -0500

    Install more poppler build dependencies

--- travis-ci/get-dependencies.sh
+++ travis-ci/get-dependencies.sh
@@ -26,8 +26,8 @@ elif [ "${TARGET_OS}" = "win" -a "${TRAVIS_OS_NAME}" = "linux" ]; then
 	sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D43A795B73B16ABE9643FE1AFD8FFF16DB45C6AB
 	print_info "Updating apt cache"
 	sudo apt-get -qq update
-	print_info "Installing packages: qt5 hunspell pkg-config"
-	sudo apt-get install -y mxe-i686-w64-mingw32.static-qt mxe-i686-w64-mingw32.static-hunspell mxe-i686-w64-mingw32.static-pkgconf
+	print_info "Installing packages: mxe-i686-w64-mingw32.static-curl freetype hunspell jpeg libpng lcms1 pkg-config qt5 tiff"
+	sudo apt-get install -y mxe-i686-w64-mingw32.static-curl mxe-i686-w64-mingw32.static-freetype mxe-i686-w64-mingw32.static-hunspell mxe-i686-w64-mingw32.static-jpeg mxe-i686-w64-mingw32.static-libpng mxe-i686-w64-mingw32.static-lcms1 mxe-i686-w64-mingw32.static-pkgconf mxe-i686-w64-mingw32.static-qt mxe-i686-w64-mingw32.static-tiff
 	print_info "Building poppler"
 	cd travis-ci
 	env PATH="/usr/lib/mxe/usr/bin:$PATH" PREFIX="/usr/lib/mxe/usr" TARGET="i686-w64-mingw32.static" make -f build-poppler-mxe.mk
