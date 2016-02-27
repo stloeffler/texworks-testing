13a22ce014f48993afc3eaa13267d237c4eb5b91
Stefan Löffler <st.loeffler@gmail.com>
2016-02-27 18:10:40 -0500

    Extract poppler non-verbose to save log space

--- travis-ci/build-poppler-mxe.mk
+++ travis-ci/build-poppler-mxe.mk
@@ -2,7 +2,7 @@ include poppler.mk
 
 poppler : $(poppler_FILE)
 	if [ "$(shell openssl dgst -sha256 '$^' 2>/dev/null)" != "SHA256($(poppler_FILE))= $(poppler_CHECKSUM)" ]; then echo "\nwrong checksum"; exit 1; fi
-	tar -xvf $^
+	tar -xf $^
 	# FIXME: patch
 	$(call poppler_BUILD,$(poppler_SUBDIR))
 
