56761ad607c1b85516c08af5345ad1d704837e9e
Stefan Löffler <st.loeffler@gmail.com>
2016-02-28 08:47:15 -0500

    Provide (and build) poppler-test

--- travis-ci/mxe/build-poppler-mxe.mk
+++ travis-ci/mxe/build-poppler-mxe.mk
@@ -4,7 +4,7 @@ poppler : $(poppler_FILE)
 	if [ "$(shell openssl dgst -sha256 '$^' 2>/dev/null)" != "SHA256($(poppler_FILE))= $(poppler_CHECKSUM)" ]; then echo "\nwrong checksum"; exit 1; fi
 	tar -xf $^
 	# FIXME: patch
-	$(call poppler_BUILD,$(poppler_SUBDIR))
+	$(call poppler_BUILD,$(poppler_SUBDIR),poppler-test)
 
 $(poppler_FILE) :
 	wget "$(poppler_URL)"
