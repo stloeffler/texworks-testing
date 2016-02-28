06fbaaf33499ff34b13b6d019017fb213f124ff7
Stefan Löffler <st.loeffler@gmail.com>
2016-02-28 09:13:30 -0500

    Add cmake configure step for windows

--- travis-ci/configure.sh
+++ travis-ci/configure.sh
@@ -21,7 +21,15 @@ if [ "${TARGET_OS}" = "linux" -a "${TRAVIS_OS_NAME}" = "linux" ]; then
 	print_info "Running CMake"
 	echo_and_run "cmake .. -DTW_BUILD_ID='travis-ci' -DCMAKE_INSTALL_PREFIX='/usr' -DDESIRED_QT_VERSION=\"$QT\""
 elif [ "${TARGET_OS}" = "win" -a "${TRAVIS_OS_NAME}" = "linux" ]; then
-	exit 1
+	print_info "Running CMake"
+	echo_and_run "cmake .. \
+		-DCMAKE_BUILD_TYPE='Release' \
+		-DTW_BUILD_ID='travis-ci' \
+		-DDESIRED_QT_VERSION=5 \
+		-DCMAKE_TOOLCHAIN_FILE='${MXEDIR}/usr/${MXETARGET}/share/cmake/mxe-conf.cmake' \
+		-DCMAKE_PREFIX_PATH='${MXEDIR}/usr/${MXETARGET}/qt5/lib/cmake' \
+		-DQTPDF_ADDITIONAL_LIBS='freetype;harfbuzz;glib-2.0;intl;iconv;ws2_32;winmm;tiff;jpeg;png;lcms;lzma;bz2' \
+		-DTEXWORKS_ADDITIONAL_LIBS='pcre16;opengl32;imm32;shlwapi'"
 elif [ "${TARGET_OS}" = "osx" -a "${TRAVIS_OS_NAME}" = "osx" ]; then
 	if [ "${QT}" -eq 4 ]; then
 		print_info "Running CMake"
