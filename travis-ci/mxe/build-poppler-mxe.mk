e0658d6fd2bc07f9fb05bf4177a296b08d97e07f
vim <st.loeffler@gmail.com>
2017-04-24 07:45:09 +0200

    MXE: Split poppler & hunspell builds (as they use different TEST_FILEs)

--- travis-ci/mxe/build-poppler-mxe.mk
+++ travis-ci/mxe/build-poppler-mxe.mk
@@ -1,7 +1,4 @@
 include poppler.mk
-include hunspell.mk
-
-all : hunspell poppler
 
 poppler : $(poppler_FILE)
 	if [ "$(shell openssl dgst -sha256 '$^' 2>/dev/null)" != "SHA256($(poppler_FILE))= $(poppler_CHECKSUM)" ]; then echo "\nwrong checksum"; exit 1; fi
@@ -9,13 +6,6 @@ poppler : $(poppler_FILE)
 	for F in poppler-*.patch; do echo "Applying $$F"; patch -d $(poppler_SUBDIR) -p1 < "$$F"; done
 	$(call poppler_BUILD,$(poppler_SUBDIR),poppler-test)
 
-hunspell : $(hunspell_FILE)
-	if [ "$(shell openssl dgst -sha256 '$^' 2>/dev/null)" != "SHA256($(hunspell_FILE))= $(hunspell_CHECKSUM)" ]; then echo "\nwrong checksum"; exit 1; fi
-	tar -xf $^
-	$(call hunspell_BUILD,$(hunspell_SUBDIR),hunspell-test)
-
 $(poppler_FILE) :
 	wget --no-check-certificate -O "$(poppler_FILE)" "$(poppler_URL)"
 
-$(hunspell_FILE) :
-	wget --no-check-certificate -O "$(hunspell_FILE)" "$(hunspell_URL)"
