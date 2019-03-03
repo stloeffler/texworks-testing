4c05740fa790f09490a2c32ec2b8ec30d7ab2a58
Stefan Löffler <st.loeffler@gmail.com>
2019-03-03 22:10:45 +0100

    Use $(shell pwd) for absolute paths

--- travis-ci/mxe/build-poppler-mxe.mk
+++ travis-ci/mxe/build-poppler-mxe.mk
@@ -4,9 +4,9 @@ poppler : $(poppler_FILE)
 	if [ "$(shell openssl dgst -sha256 '$^' 2>/dev/null)" != "SHA256($(poppler_FILE))= $(poppler_CHECKSUM)" ]; then echo "\nwrong checksum"; exit 1; fi
 	tar -xf $^
 	for F in poppler-*.patch; do echo "Applying $$F"; patch -d $(poppler_SUBDIR) -p1 < "$$F"; done
-	$(eval SOURCE_DIR := $(poppler_SUBDIR))
+	$(eval SOURCE_DIR := $(shell pwd)/$(poppler_SUBDIR))
 	$(eval BUILD_DIR := $(poppler_SUBDIR)/build)
-	$(eval TEST_FILE := $(pwd)/poppler-test.cxx)
+	$(eval TEST_FILE := $(shell pwd)/poppler-test.cxx)
 	mkdir $(BUILD_DIR)
 	$(call poppler_BUILD)
 
