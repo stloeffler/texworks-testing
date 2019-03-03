d52f3935501973b768b8f9133d69e360d7ae552b
Stefan Löffler <st.loeffler@gmail.com>
2019-03-03 21:52:21 +0100

    Migrate build-poppler-mxe.mk to new MXE Makefile conventions

--- travis-ci/mxe/build-poppler-mxe.mk
+++ travis-ci/mxe/build-poppler-mxe.mk
@@ -4,7 +4,10 @@ poppler : $(poppler_FILE)
 	if [ "$(shell openssl dgst -sha256 '$^' 2>/dev/null)" != "SHA256($(poppler_FILE))= $(poppler_CHECKSUM)" ]; then echo "\nwrong checksum"; exit 1; fi
 	tar -xf $^
 	for F in poppler-*.patch; do echo "Applying $$F"; patch -d $(poppler_SUBDIR) -p1 < "$$F"; done
-	$(call poppler_BUILD,$(poppler_SUBDIR),poppler-test)
+	BUILD_DIR=$(poppler_SUBDIR)/build
+	TEST_FILE=$(pwd)/poppler-test.cxx
+	mkdir $(BUILD_DIR)
+	$(call poppler_BUILD)
 
 $(poppler_FILE) :
 	wget --no-check-certificate -O "$(poppler_FILE)" "$(poppler_URL)"
