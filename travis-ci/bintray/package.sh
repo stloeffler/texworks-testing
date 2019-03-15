cc5b90a34d831119f9632357410462bbdf5529b9
Stefan Löffler <st.loeffler@gmail.com>
2019-03-15 08:23:47 +0100

    Include seconds in GIT_DATE_HASH and use similar formatting as Makefile

--- travis-ci/bintray/package.sh
+++ travis-ci/bintray/package.sh
@@ -9,7 +9,7 @@ print_headline "Packaging for Bintray"
 
 GIT_HASH=$(git --git-dir=".git" show --no-patch --pretty="%h")
 GIT_DATE=$(git --git-dir=".git" show --no-patch --pretty="%ci")
-GIT_DATE_HASH=$(date -u +"%Y%m%d%H%M" --date="${GIT_DATE}")
+GIT_DATE_HASH=$(echo "$GIT_DATE" | tr -d '-:')
 DATE_HASH=$(date -u +"%Y%m%d%H%M")
 VERSION="0.1.1"
 VERSION_NAME="${VERSION}-${DATE_HASH}-git_${GIT_HASH}"
