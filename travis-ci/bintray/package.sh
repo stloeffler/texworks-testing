67069c53fd97c39783c5ca48c2c90dcc266d2a2a
Stefan Löffler <st.loeffler@gmail.com>
2019-03-15 22:30:23 +0100

    reverse bintray file order

--- travis-ci/bintray/package.sh
+++ travis-ci/bintray/package.sh
@@ -39,8 +39,8 @@ cat > "${TRAVIS_BUILD_DIR}/travis-ci/bintray/bintray.json" <<EOF
 	},
 	"files":
 	[
-		{"includePattern": "pdf/([-_a-zA-Z]+)/TeXworks-manual-([-_a-zA-Z]+).pdf", "uploadPattern": "TeXworks-manual-${VERSION_NAME}-\$1.pdf"},
-		{"includePattern": "html/TeXworks-manual-html-(\d+)-${GIT_HASH}.zip", "uploadPattern": "TeXworks-manual-\$1-${VERSION_NAME}-html.zip"}
+		{"includePattern": "html/TeXworks-manual-html-(\d+)-${GIT_HASH}.zip", "uploadPattern": "TeXworks-manual-\$1-${VERSION_NAME}-html.zip"},
+		{"includePattern": "pdf/([-_a-zA-Z]+)/TeXworks-manual-([-_a-zA-Z]+).pdf", "uploadPattern": "TeXworks-manual-${VERSION_NAME}-\$1.pdf"}
 	],
 	"publish": true
 }
