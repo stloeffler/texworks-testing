f8741aa7d46ba5adad50b0c688cca3ed94448b1a
Stefan Löffler <st.loeffler@gmail.com>
2020-04-16 18:38:20 +0200

    Set OpenJPEG_DIR

--- .github/workflows/build-poppler.py
+++ .github/workflows/build-poppler.py
@@ -6,6 +6,7 @@ POPPLER_FILENAME='poppler-{0}.tar.xz'.format(POPPLER_VERSION)
 POPPLER_URL='https://poppler.freedesktop.org/{0}'.format(POPPLER_FILENAME)
 
 OPENJPEG_VERSION = '2.3.1'
+OPENJPEG_CMAKE_DIR = os.path.join(os.environ.get('VCPKG_INSTALLATION_ROOT'), 'packages', 'openjpeg-v{0}-windows-x64'.format(OPENJPEG_VERSION), 'lib', 'openjpeg-2.3')
 OPENJPEG_FILENAME = 'openjpeg-v{0}-windows-x64.zip'.format(OPENJPEG_VERSION)
 OPENJPEG_URL = 'https://github.com/uclouvain/openjpeg/releases/download/v{0}/{1}'.format(OPENJPEG_VERSION, OPENJPEG_FILENAME)
 
@@ -25,6 +26,6 @@ downloadFile(POPPLER_FILENAME, POPPLER_URL)
 # FIXME: Check checksum
 echo_and_run(['7z', 'x', POPPLER_FILENAME])
 echo_and_run(['7z', 'x', os.path.splitext(POPPLER_FILENAME)[0]])
-echo_and_run(['cmake', '-B', 'build', '-DCMAKE_TOOLCHAIN_FILE={0}'.format(os.path.join(os.environ.get('VCPKG_INSTALLATION_ROOT'), 'scripts/buildsystems/vcpkg.cmake')), '.'], cwd = POPPLER_FOLDER)
+echo_and_run(['cmake', '-B', 'build', '-DOpenJPEG_DIR={0}'.format(OPENJPEG_CMAKE_DIR)), '-DCMAKE_TOOLCHAIN_FILE={0}'.format(os.path.join(os.environ.get('VCPKG_INSTALLATION_ROOT'), 'scripts/buildsystems/vcpkg.cmake')), '.'], cwd = POPPLER_FOLDER)
 echo_and_run(['cmake', '--build', 'build'], cwd = POPPLER_FOLDER)
 echo_and_run(['cmake', '--install', 'build'], cwd = POPPLER_FOLDER)
