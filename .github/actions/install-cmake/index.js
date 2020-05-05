e60e8d2a760213af144466f2f3d42dbc520e312f
Stefan Löffler <st.loeffler@gmail.com>
2020-05-05 19:46:39 +0200

    install-cmake action

---
+++ .github/actions/install-cmake/index.js
@@ -0,0 +1,48 @@
+const core = require('@actions/core');
+const tc = require('@actions/tool-cache');
+
+async function extract(archive) {
+	if (process.platform === 'win32') {
+		return await tc.extractZip(archive);
+	} else {
+		return await tc.extractTar(archive);
+	}
+}
+
+async function run() {
+	try {
+		const version = core.getInput('version');
+		const fullname = function() {
+			switch (process.platform) {
+				case 'darwin':
+					return `cmake-${version}-Darwin-x86_64`;
+				case 'win32':
+					return `cmake-${version}-win64-x64`;
+				case 'linux':
+				default:
+					return `cmake-${version}-Linux-x86_64`;
+			}
+		}();
+		const archive = function() {
+			switch (process.platform) {
+				case 'win32':
+					return `${fullname}.zip`;
+				default:
+					return `${fullname}.tar.gz`;
+			}
+		}();
+		const url = `https://github.com/Kitware/CMake/releases/download/v${version}/${archive}`;
+
+		console.log(`Downloading CMake from ${url}`);
+		const archivePath = await tc.downloadTool(url);
+
+		core.startGroup('Extracting');
+		const folder = await extract(archivePath);
+		core.addPath(`${folder}/${fullname}/bin`);
+		core.endGroup();
+	} catch(error) {
+		core.setFailed(error.message);
+	}
+}
+
+run();
