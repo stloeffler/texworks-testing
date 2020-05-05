b44b3eec75a6a7b747ac930783bfe746b30fb67f
Stefan Löffler <st.loeffler@gmail.com>
2020-05-05 20:29:48 +0200

    platform-specific path to cmake

--- .github/actions/install-cmake/index.js
+++ .github/actions/install-cmake/index.js
@@ -13,23 +13,14 @@ async function extract(archive) {
 async function run() {
 	try {
 		const version = core.getInput('version');
-		const fullname = function() {
-			switch (process.platform) {
-				case 'darwin':
-					return `cmake-${version}-Darwin-x86_64`;
-				case 'win32':
-					return `cmake-${version}-win64-x64`;
-				case 'linux':
-				default:
-					return `cmake-${version}-Linux-x86_64`;
-			}
-		}();
 		const archive = function() {
 			switch (process.platform) {
+				case 'darwin':
+					return `cmake-${version}-Darwin-x86_64.tar.gz`;
 				case 'win32':
-					return `${fullname}.zip`;
-				default:
-					return `${fullname}.tar.gz`;
+					return `cmake-${version}-win64-x64.zip`;
+				case 'linux':
+					return `cmake-${version}-Linux-x86_64.tar.gz`;
 			}
 		}();
 		const url = `https://github.com/Kitware/CMake/releases/download/v${version}/${archive}`;
@@ -39,9 +30,19 @@ async function run() {
 
 		core.startGroup('Extracting');
 		const folder = await extract(archivePath);
-		core.addPath(`${folder}/${fullname}/bin`);
+		const pathToCMake = function() {
+			switch (process.platform) {
+				case 'darwin':
+					return `${folder}/cmake-${version}-Darwin-x86_64/CMake.app/Contents/bin`;
+				case 'win32':
+					return `${folder}/cmake-${version}-win64-x64/bin`;
+				case 'linux':
+					return `${folder}/cmake-${version}-Linux-x86_64/bin`;
+			}
+		}();
+		core.addPath(pathToCMake);
 
-		console.log(`Adding CMake to path: ${folder}/${fullname}/bin`);
+		console.log(`Adding CMake to path: ${pathToCMake}`);
 
 		if (process.platform === 'win32') {
 			await exec.exec('msys2do', ['find', folder]);
