0afda192d1e2b187f093c66b6af0414c46771be6
Stefan Löffler <st.loeffler@gmail.com>
2020-06-19 16:09:50 +0200

    Fix ;

--- sysinfo.mm
+++ sysinfo.mm
@@ -4,5 +4,5 @@
 QString getOSVersionString() {
 	auto osv = [[NSProcessInfo processInfo] operatingSystemVersion];
 
-	return QStringLiteral("Mac OS X %1.%2.%3").arg(osv.majorVersion).arg(osv.minorVersion).arg(osv.patchVersion)
+	return QStringLiteral("Mac OS X %1.%2.%3").arg(osv.majorVersion).arg(osv.minorVersion).arg(osv.patchVersion);
 }
