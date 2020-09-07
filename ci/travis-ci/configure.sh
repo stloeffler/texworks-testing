4d015b17f1a4dc5b87e8c47b5c10695ec07fbf06
Stefan Löffler <st.loeffler@gmail.com>
2020-09-07 19:10:41 +0200

    Move MXE libs from ADDITIONAL_LIBS to PLATFORM_DEPENDENCIES

--- ci/travis-ci/configure.sh
+++ ci/travis-ci/configure.sh
@@ -36,9 +36,9 @@ elif [ "${TARGET_OS}" = "win" -a "${TRAVIS_OS_NAME}" = "linux" ]; then
 	print_info "Running CMake"
 	echo_and_run "${MXEDIR}/usr/bin/${MXETARGET}-cmake .. \
 		${CMAKE_OPTS} \
-		-DPLATFORM_DEPENDENCIES='freetype;harfbuzz_too;freetype;bz2' \
+		-DPLATFORM_DEPENDENCIES='freetype;harfbuzz_too;freetype;bz2;iphlpapi;crypto' \
 		-DPoppler_ADDITIONAL_DEPENDENCIES='freetype;harfbuzz;freetype_too;glib-2.0;intl;iconv;ws2_32;winmm;tiff;webp;jpeg;openjp2;png;lcms2;lzma;bz2;pcre16;wtsapi32' \
-		-DTEXWORKS_ADDITIONAL_LIBS='wtsapi32;opengl32;imm32;shlwapi;dwmapi;uxtheme;iphlpapi;crypto' \
+		-DTEXWORKS_ADDITIONAL_LIBS='wtsapi32;opengl32;imm32;shlwapi;dwmapi;uxtheme' \
 		-Dgp_tool='none'"
 	if [ -f "CMakeFiles/CMakeError.log" ]; then
 		echo "=== CMake Error Log ==="
