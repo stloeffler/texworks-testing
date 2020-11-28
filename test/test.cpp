26352aa2f21f22a5fe5a78073581a213116e0779
Stefan Löffler <st.loeffler@gmail.com>
2020-11-28 08:51:26 +0100

    Switch back to non-Qt test

--- test/test.cpp
+++ test/test.cpp
@@ -1,8 +1,16 @@
+/*
 #include <QApplication>
 #include <QMessageBox>
 
 int main(int argc, char * argv[]) {
 	QApplication app(argc, argv);
 	QMessageBox::information(nullptr, QStringLiteral("It works!"), QStringLiteral("Hello World"));
+	return 0;
+}
+*/
+
+#include <iostream>
+int main() {
+	std::cout << "Hello World\n";
 	return 0;
 }
