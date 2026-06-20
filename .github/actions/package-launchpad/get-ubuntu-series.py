b663c808b36b27d13a1804f15f80e3e183236e30
Stefan Löffler <st.loeffler@gmail.com>
2026-06-20 06:58:59 +0200

    Use python3-distro-info

--- .github/actions/package-launchpad/get-ubuntu-series.py
+++ .github/actions/package-launchpad/get-ubuntu-series.py
@@ -1,5 +1,10 @@
-import json, urllib.request
+#import json, urllib.request
+#
+#with urllib.request.urlopen('https://api.launchpad.net/1.0/ubuntu/series') as www:
+#	dat = json.loads(www.read())
+#print(' '.join([e['name'] for e in dat['entries'] if e['active']]))
 
-with urllib.request.urlopen('https://api.launchpad.net/1.0/ubuntu/series') as www:
-	dat = json.loads(www.read())
-print(' '.join([e['name'] for e in dat['entries'] if e['active']]))
+from distro_info import UbuntuDistroInfo
+
+info = UbuntuDistroInfo()
+print(' '.join(sorted(set(info.supported() + info.supported_esm()))))
